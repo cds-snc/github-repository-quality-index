f6fe086c92a1d4402dee77eaa8b2006ea01f8448
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2019-06-21 15:15:00 -0400

    numCommits was exporting the wrong type (#23)

--- agent/src/lib/numCommits.ts
+++ agent/src/lib/numCommits.ts
@@ -1,4 +1,4 @@
-export const numCommits = async (client: any, name: string, numDays: number): Promise<boolean|null> => {
+export const numCommits = async (client: any, name: string, numDays: number): Promise<number|null> => {
   try {
     let date= +new Date();
     let pastDate = new Date(date - 1000 * 60 * 60 * 24 * numDays);
