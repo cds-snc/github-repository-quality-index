89a2c73b30b07a6e238ee0ea4ff936fa6fb4e5ab
Tim Arney <tim@line37.com>
2019-06-24 14:33:30 -0400

    add sample connect + write

--- db/index.js
+++ db/index.js
@@ -1,25 +1,25 @@
+// this file is for testing only
+const Repo = require("./models/repo");
 const Sequelize = require("sequelize");
-
 const { configure } = require("sequelize-pg-utilities");
-const config = require("./config/config.json");
+const config = require("./config/config.js");
 const { name, user, password, options } = configure(config);
-
-// npx sequelize-cli db:create
-// npx sequelize-cli db:migrate
-// npx sequelize-cli db:seed:all
-
-
 const sequelize = new Sequelize(name, user, password, options);
 
-sequelize.createDatabase("new-database");
-
-/*
-sequelize
-  .authenticate()
-  .then(() => {
+(async () => {
+  try {
+    await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-  })
-  .catch(err => {
-    console.error("Unable to connect to the database:", err);
-  });
-  */
+    /*
+    const repos = await Repo(sequelize, Sequelize);
+    const data = await repos.findOne();
+    repos.create({
+      repoName: "works",
+      statistics: { pull_requests: 8, approved: true }
+    });
+    console.log("All users:", JSON.stringify(data, null, 4));
+    */
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
